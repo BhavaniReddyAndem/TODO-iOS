7ceedbc59a48f333f8547404c91e9919bf32ec30
BhavaniReddyAndem <bhavanireddyandem@gmail.com>
2024-04-29 12:20:03 -0500

    first commit

---
+++ ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Bhavani Reddy Navure on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
